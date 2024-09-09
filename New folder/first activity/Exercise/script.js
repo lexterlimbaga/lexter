1b33ae939ed2e418aa955cc196a147f4612c58a9
unknown <lexterlimbaga4@gmail.com>
2024-09-09 13:37:17 +0800

    lab actibity1

---
+++ New folder/first activity/Exercise/script.js
@@ -0,0 +1,60 @@
+console.log("This is an external js");
+// let name = prompt("What is you name?");
+// console.log(name);
+
+var number = 3;
+console.log(number);
+number = "lexter";
+console.log(number);
+
+const PI=3.1415;
+console.log(PI);
+
+// add +
+// sub -
+// mul *
+// div /
+// mod %
+
+// let num1 = prompt("Emter Number 1");
+// let num2 = prompt("Emter Number 2");
+
+// let sum = parseInt(num1)+parseInt(num2);
+// console.log("The sum is:"+sum);
+
+// let difference = parseInt(num1)-parseInt(num2);
+// console.log("The difference is:"+difference);
+
+// let product = parseInt(num1)*parseInt(num2);
+// console.log("The product is:"+product);
+
+// let quotient = parseInt(num1)/parseInt(num2);
+// console.log("The quotient is:"+quotient);
+
+// let desimal = parseInt(num1)%parseInt(num2);
+// console.log("The desimal is:"+desimal);
+
+let name = "lexter"
+let age=20;
+console.log("Your name is "+name+". Your age is");
+console.log("Your name is ${name}, Your age is ${age}");
+
+// let a=5;
+// let b = '5';
+
+// console.log(a===b);
+
+let a=5;
+let b=5;
+
+ if(a==b){
+    console.log("Equals");
+ }
+ else if (a!=b){
+    console.log("Not Equals");
+ }
+ if(a>b){
+    console.log("Greater Than");
+ }else if(a<b){
+    console.log("Less Than");
+ }
---
+++ New folder/lab activity 1/script.js
@@ -0,0 +1,13 @@
+let q1 = prompt("Enter Quiz 1: ")
+  console.log("Quiz 1: " + q1)
+
+let q2 = prompt("Enter Quiz 2: ")
+  console.log("Quiz 2: " + q2)
+
+let q3 = prompt("Enter Quiz 3: ")
+  console.log("Quiz 3:" + q3)
+
+let ave = parseInt(q1) + parseInt(q2) + parseInt(q3)
+let ave2 = ave/3
+console.log("QUiz average" + ave2)
+
